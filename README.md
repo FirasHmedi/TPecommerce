c026eece016919dcbc279f6924129a6578c465ea
MokhtarNajjar <45322274+MokhtarNajjar@users.noreply.github.com>
2019-12-07 13:13:21 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Tpecommerce
+# Appppp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.19.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.12.
 
 ## Development server
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/tpecommerce'),
+      dir: require('path').join(__dirname, './coverage/appppp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
