76673306bdf6e404e5fbc5e214be968a11671ec3
MokhtarNajjar <45322274+MokhtarNajjar@users.noreply.github.com>
2019-12-09 10:52:39 +0100

    Update filter.pipe.ts

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,20 +6,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, SearchStatus:string,name:string,prix:number,PrixStatus:number): any {
-    if(value.length === 0 || (SearchStatus==='' )&&(PrixStatus<=0))
+    if(value.length === 0 || ((SearchStatus==='')&&(PrixStatus===null)))
     {
       return value ; 
     }
 
     const FilterResult=[];
     for(const item of value){
-      if(((item[name].indexOf(SearchStatus))!=-1)&&(item.prix<=PrixStatus))
+      if(((item[name].indexOf(SearchStatus))!=-1)&&((item.prix<=PrixStatus)||((PrixStatus===null))))
       {
         FilterResult.push(item);
+        
       }
+      console.log(PrixStatus);
     }
-    if(FilterResult.length!=0)
-    {return FilterResult;}
+    
+    return FilterResult;
     
   
 }
